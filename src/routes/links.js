a42b09cd7471c95b2bcc8d8d12a3b01d3796f7df
emman-AG <manolo29@live.com.mx>
2020-07-14 14:58:51 -0500

    eliminar crads etc

--- src/routes/links.js
+++ src/routes/links.js
@@ -17,7 +17,20 @@ router.post('/ad', async (req,res) =>{
         description
     };    
     await db.query('INSERT INTO links set ?', [newLinks]);
-    res.send('recived');
+    //redirecciona a la pagina de las tarjetas
+    res.redirect("/links");
+});
+
+router.get('/', async (req, res) => {
+    const links = await db.query('SELECT * FROM links');
+    //mandamos los datos de la tabla a links/list
+    res.render('links/list', { links });
+});
+
+router.get('/delete/:id', async (req, res) => {
+    const { id } = req.params;
+    await db.query('DELETE FROM links WHERE ID = ?', [id]);
+    res.redirect('/links');
 });
 
 module.exports = router;
